625d7506606a3ca3fbf5c19ab76c3e36e0944165
olaniyigd <106377922+olaniyigd@users.noreply.github.com>
2022-10-17 10:06:56 +0100

    Add files via upload

---
+++ aj.js
@@ -0,0 +1,100 @@
+const countdown = () => {
+  const countDate = new Date ("feb 25, 2023 00:00:00").getTime()
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor (gap / day);
+  const textHour = Math.floor ((gap % day)/ hour);
+  const textMinute = Math.floor ((gap % hour)/ minute);
+  const textSecond = Math.floor ((gap % minute)/ second);
+
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".minute").innerText = textMinute;
+  document.querySelector(".second").innerText = textSecond;
+};
+setInterval(countdown, 1000);
+
+
+window.onscroll = function (){
+  var count = 0;
+  var project1 = setInterval(project,10);
+  
+  function project(){
+      count++;
+      
+      var num1 = document.getElementById("num1").innerHTML=count;
+  
+  
+      if(count==50){
+          clearInterval(project1);
+      }
+  }
+
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
